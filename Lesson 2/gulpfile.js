44f21e2c5b9e06bfbc96209390c963042d252391
Dami <obayanjud@gmail.com>
2018-09-20 19:40:36 -0600

    Implement css autoprefixer and gulp watcher

--- Lesson 2/gulpfile.js
+++ Lesson 2/gulpfile.js
@@ -1,14 +1,22 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 // this task is just a function
-gulp.task("styles", function(done) {
+gulp.task("styles", function() {
   return gulp
     .src("sass/**/*.scss")
     .pipe(sass())
     .on("error", sass.logError)
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 version"]
+      })
+    )
     .pipe(gulp.dest("./css"));
 });
+
+gulp.watch("sass/**/*.scss", gulp.series("styles"));
 
 // function defaultTask(cb) {
 //   // place code for your default task here
